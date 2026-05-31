6491d57dde120eb698e488babfbcab6d17aa7334
root <root@openclaw.kdnfx.space>
2026-05-31 13:55:44 +0000

    chore: bump version to 1.6.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.polinalinen.madre"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.6.4"
+        versionCode = 11
+        versionName = "1.6.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
